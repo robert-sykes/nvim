7086bea7d9eb0b706d7d15d434d6c2b86c6498c2
robert-sykes <me@robsykes.dev>
2025-12-09 09:25:30 +0000

    theme update

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme tokyonight-day") -- light theme
+      vim.cmd("colorscheme tokyonight-night")
     end,
   },
 }
